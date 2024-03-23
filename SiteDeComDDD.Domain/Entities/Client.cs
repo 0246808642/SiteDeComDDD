5c3896e6c04f16115d7c8380e653a72fdadddeb5
caique  coelho pinheiro <caiquepinheiro69@gmai.com>
2024-03-23 11:19:56 -0300

    Add project files.

---
+++ SiteDeComDDD.Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace SiteDeComDDD.Domain.Entities
+{
+    public class Client
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Client client)
+        {
+            return client.Ativo && DateTime.Now.Year - client.DataCadastro.Year >= 5;
+        }
+    }
+}
---
+++ SiteDeComDDD.Infra.Data/Context/MeuSiteContext.cs
@@ -0,0 +1,19 @@
+﻿
+using SiteDeComDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace SiteDeComDDD.Infra.Data.Context
+{
+    public class MeuSiteContext: DbContext
+    {
+
+        public MeuSiteContext()
+            : base("MeuSiteStringDeConexao")
+        {
+
+        }
+
+        public DbSet<Client> Cliente { get; set; }
+    }
+    
+}
